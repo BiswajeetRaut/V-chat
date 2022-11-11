c8159cfa2f9b7b235894daa0f7bf1e27db9f6339
BiswajeetRaut <85820671+BiswajeetRaut@users.noreply.github.com>
2022-11-12 02:57:09 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,13 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 import firebase from "firebase";
 const firebaseConfig = {
-    apiKey: "AIzaSyAGIuI1DSUPQbCp-fHcpGOswXMgesrnmmc",
-    authDomain: "v-chat-app-7f499.firebaseapp.com",
-    projectId: "v-chat-app-7f499",
-    storageBucket: "v-chat-app-7f499.appspot.com",
-    messagingSenderId: "692424367225",
-    appId: "1:692424367225:web:067df0b16be59c321308a8",
-    measurementId: "G-FXSVS8GCCC"
+    apiKey: ,
+    authDomain: ,
+    projectId: ,
+    storageBucket: ,
+    messagingSenderId: ,
+    appId: ,
+    measurementId: 
   };
   const firebaseApp = firebase.initializeApp(firebaseConfig);
   const db=firebaseApp.firestore();
